54ca61249e47723ef43b843ef12ca93b7027fb20
razinshafayet <sumonislamk29@gmail.com>
2025-05-14 13:13:54 +0600

    Changes in lightmode

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ const themeBtn = document.getElementById('themeBtn');
 const menuList = document.getElementById('menuList');
 const lightOption = document.getElementById('lightOption');
 const darkOption = document.getElementById('darkOption');
+const themeIcon = document.getElementById('themeIcon');
 
 themeBtn.addEventListener('click', (e) => {
     e.stopPropagation();
@@ -43,11 +44,13 @@ themeBtn.addEventListener('click', (e) => {
 
 lightOption.addEventListener('click', () => {
     document.documentElement.classList.add('lightmode');
+    themeIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>'
     menuList.classList.add('hidden');
 });
 
 darkOption.addEventListener('click', () => {
     document.documentElement.classList.remove('lightmode');
+    themeIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-star-icon lucide-moon-star"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9"/><path d="M20 3v4"/><path d="M22 5h-4"/></svg>'
     menuList.classList.add('hidden');
 });
 
